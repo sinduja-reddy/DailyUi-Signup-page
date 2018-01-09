1dbfeaf76a908b5dde556d8cc3af22970e43e5d7
sindu <sindu0225@gmail.com>
2018-01-09 15:23:46 +0100

    fix errors

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class Signup extends Component {
 }
 
 
-onChange(event) {
+	onChange(event) {
 		const state=this.state;
 		state.errorText=''
 		state[event.target.name]= event.target.value 
@@ -43,39 +43,38 @@ onChange(event) {
       	 state[event.target.name+'ErrorText']=''
 
       }
-}
- handleSubmit(e){
- 	const state=this.state;
- 	e.preventDefault();
-	let email = this.state.email.trim();
-	let username = this.state.username.trim();
-	let password=this.state.password.trim();
-	if (email===''|| username===""|| password==="") {
-		state.errorText='Fill the form'
-		this.setState({state})
-	}else{
-	alert('submited');
 	}
+ 	handleSubmit(e){
+ 		const state=this.state;
+ 		e.preventDefault();
+		let email = this.state.email.trim();
+		let username = this.state.username.trim();
+		let password=this.state.password.trim();
+		if (email===''|| username===""|| password==="") {
+			state.errorText='Fill the form'
+			this.setState({state})
+		}else{
+			alert('submited');
+		}
 // TODO: send request to the server
-
- }
-  render() {
-    return (
-      <div className="App">
-      <div className="appItems">
-     <RaisedButton label="SIGN UP WITH FACEBOOK " primary={true} className="button"fullWidth={true} />
-      <span className='or'>or</span>
-      <h4>Sing up with your email address</h4>
-      	<form onSubmit={this.handleSubmit}>
-    	<TextField hintText="email" name= "email" type="email" value={this.state.email} floatingLabelText="Email" errorText= {this.state.emailErrorText}  onChange={this.onChange.bind(this)} underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
-    	 <TextField hintText="Password" name="password" value={this.state.password} type="password"  errorText= {this.state.passwordErrorText} floatingLabelText="Password" onChange={this.onChange.bind(this)}  underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
-		<TextField hintText="User name"  name="username" value={this.state.username} floatingLabelText="User name" onChange={this.onChange.bind(this)} errorText= {this.state.usernameErrorText} underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
-		<RaisedButton label="Sign up" type="submit" secondary={true} className="signupButton"  fullWidth={true}/>
-		<p>{this.state.errorText}</p>
-	  </form>
-	  </div>
-      </div>
-    );
+}
+  	render() {
+    	return (
+      	<div className="App">
+      		<div className="appItems">
+     			<RaisedButton label="SIGN UP WITH FACEBOOK " primary={true} className="button"fullWidth={true} />
+      			<span className='or'>or</span>
+      			<h4>Sing up with your email address</h4>
+      			<form onSubmit={this.handleSubmit}>
+    				<TextField hintText="email" name= "email" type="email" value={this.state.email} floatingLabelText="Email" errorText= {this.state.emailErrorText}  onChange={this.onChange.bind(this)} underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
+    	 			<TextField hintText="Password" name="password" value={this.state.password} type="password"  errorText= {this.state.passwordErrorText} floatingLabelText="Password" onChange={this.onChange.bind(this)}  underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
+					<TextField hintText="User name"  name="username" value={this.state.username} floatingLabelText="User name" onChange={this.onChange.bind(this)} errorText= {this.state.usernameErrorText} underlineFocusStyle={styles.underlineFocusStyle} floatingLabelFocusStyle={styles.floatingLabelFocusStyle}  underlineStyle={styles.underlineStyle}/>
+					<RaisedButton label="Sign up" type="submit" secondary={true} className="signupButton"  fullWidth={true}/>
+					<p>{this.state.errorText}</p>
+	  			</form>
+	  		</div>
+      	</div>
+    	);
 	}
  }
 
